c9e2fb8c52e45eeb877f57f5d02943749727887e
AJB <none@none>
2010-12-01 18:17:01 +0000

    Added another sink to Switching

--- Switching/Switching.cpp
+++ Switching/Switching.cpp
@@ -6,6 +6,7 @@
 #include <cryptlib.h>
 #include <filters.h>
 #include <channels.h>
+#include <hex.h>
 
 #include <iostream>
 #include <string>
@@ -20,21 +21,32 @@ int _tmain(int argc, _TCHAR* argv[])
 	cout << "Enter a string: ";
 	cin.getline(instr, 25);
 
-	cout << instr << endl;
+	// Display the string as entered, and in ASCII
+	cout << instr << " --> ";
+	for (size_t x = 0; x < strlen(instr); x++)
+	{
+		cout << (int)instr[x] << " ";
+	}
+	cout << endl;
 
+	// Send the string down three different paths from a single source
 	string out1 = "ONE_";
 	string out2 = "TWO_";
+	string out3 = "THREE_";
 
 	StringSink sink1(out1);
 	StringSink sink2(out2);
+	HexEncoder sink3(new StringSink(out3),true,2,":");
 
 	ChannelSwitch *cs = new ChannelSwitch(sink1);
 	cs->AddDefaultRoute(sink2);
+	cs->AddDefaultRoute(sink3);
 
 	StringSource(instr,true,cs);
 
 	cout << out1 << endl;
 	cout << out2 << endl;
+	cout << out3 << endl;
 
 	return 0;
 }
